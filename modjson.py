6ae28184e4e029b99bb37bdd2bb4c08728aaa771
David Bradway <dpb6@duke.edu>
2018-09-28 10:47:40 -0400

    add type hints and change variable names

--- modjson.py
+++ modjson.py
@@ -1,43 +1,41 @@
 import json
+from typing import Dict, Any
 
-def create_file(filename):
-    data = {'firstname': 'Scott', 'from': 'Nebraska'}
-    write_file(data, filename)
-
-def write_file(data, filename):
+def write_file(filepath: str, data: Dict) -> None:
     # Write data to a json file
-    with open(filename, 'w') as outfile:  
+    with open(filepath, 'w') as outfile:
         json.dump(data, outfile, indent=4)
 
-def read_file(filename):
-    with open(filename, 'r') as json_file:  
+def read_file(filepath: str) -> Dict:
+    with open(filepath, 'r') as json_file:
         data = json.load(json_file)
         return data
 
-def display_data(data):
+def display_data(data: Dict) -> None:
     print(data)
 
-def modify(data, name, value):
+def modify(data: Dict, name: str, value: Any) -> Dict:
     data[name] = value
     return data
 
-def modify_file_dict(filename, d):
+def modify_file_dict(filepath: str, d: Dict) -> None:
     # Read data in
-    data = read_file(filename)
+    data = read_file(filepath)
     display_data(data)
     # change some field values
     for key, value in d.items():
         data = modify(data, key, value)
         data = modify(data, 'from', 'Ohio')
     display_data(data)
-    write_file(data, filename)
+    write_file(filepath, data)
 
-def main():
-    abolute_path = r'C:\Users\dpb6\Downloads\repos\modjson\data.txt'
-    create_file(abolute_path)
+def main() -> None:
+    filepath = r'C:\Users\dpb6\Downloads\repos\modjson\data.txt'
+    data = {'firstname': 'Scott', 'from': 'Nebraska'}
+    write_file(filepath, data)
     # Create dictionary of name-value pairs to modify
-    d = {'firstname': 'David', 'from': 'Ohio'}
-    modify_file_dict(abolute_path, d)
+    data = {'firstname': 'David', 'from': 'Ohio'}
+    modify_file_dict(filepath, data)
 
 if __name__ == '__main__':
     main()
