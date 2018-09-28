8777e17a4a1d5c1fe39266c4c7402db171396852
David Bradway <dpb6@duke.edu>
2018-09-28 10:31:27 -0400

    add absolute path and dict of name-val pairs

--- modjson.py
+++ modjson.py
@@ -7,10 +7,10 @@ def create_file(filename):
 def write_file(data, filename):
     # Write data to a json file
     with open(filename, 'w') as outfile:  
-        json.dump(data, outfile)
+        json.dump(data, outfile, indent=4)
 
 def read_file(filename):
-    with open(filename) as json_file:  
+    with open(filename, 'r') as json_file:  
         data = json.load(json_file)
         return data
 
@@ -21,17 +21,23 @@ def modify(data, name, value):
     data[name] = value
     return data
 
-def main():
-    filename = 'data.txt'
-    create_file(filename)
+def modify_file_dict(filename, d):
     # Read data in
     data = read_file(filename)
     display_data(data)
     # change some field values
-    data = modify(data, 'firstname', 'David')
-    data = modify(data, 'from', 'Ohio')
+    for key, value in d.items():
+        data = modify(data, key, value)
+        data = modify(data, 'from', 'Ohio')
     display_data(data)
     write_file(data, filename)
+
+def main():
+    abolute_path = r'C:\Users\dpb6\Downloads\repos\modjson\data.txt'
+    create_file(abolute_path)
+    # Create dictionary of name-value pairs to modify
+    d = {'firstname': 'David', 'from': 'Ohio'}
+    modify_file_dict(abolute_path, d)
 
 if __name__ == '__main__':
     main()
